da623b73989bb1b5b75947b5a4425ecbe5ec037f
Irene-Lim <irenelim1027@gmail.com>
2021-01-29 22:57:41 -0800

    grocery

---
+++ src/components/Basket.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Basket(props){
+    return(
+     <aside className="block col-1">
+         <h2>Cart Items</h2>
+     </aside>
+    )
+}
+export default Basket;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products:[
+        {
+            id:'1',
+            name:'Apple',
+            price:0.99,
+            image:'https://www.applesfromny.com/wp-content/uploads/2020/05/Jonagold_NYAS-Apples2.png',
+
+        },
+        {
+            id:'2',
+            name:'Banana',
+            price:1.98,
+            image:'https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg',
+
+        },
+        {
+            id:'3',
+            name:'Grape',
+            price:3.97,
+            image:'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/323266_1100-1100x628.jpg',
+
+        },
+
+        {
+            id:'4',
+            name:'Strawberry',
+            price:4.99,
+            image:'https://images-na.ssl-images-amazon.com/images/I/71olqgmEkHL._AC_SX679_.jpg',
+
+        },
+        {
+            id:'5',
+            name:'Blueberry',
+            price:2.97,
+            image:'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/287/287710/blueberries.jpg?w=1155&h=1541',
+
+        },
+    ],
+};
+export default data;
